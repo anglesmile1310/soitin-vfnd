befad7bfe7579ac5d325e52eb5f590b515007b89
Unknown <thanh.hoquangcse96@gmail.com>
2019-01-06 03:09:37 +0700

    update

--- app.py
+++ app.py
@@ -85,5 +85,4 @@ def show_subpath(subpath):
 if __name__ == '__main__':
   app.run(debug = True)
   # Bind to PORT if defined, otherwise default to 5000.
-  port = int(os.environ.get('PORT', 5000))
-  app.run(host='0.0.0.0', port=port)
+  app.run()
