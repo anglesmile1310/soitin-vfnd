4911b2559dc424b2fe2a80c8c3187a56eee04282
Thanh Ho Quang <thanh.hoquangcse96@gmail.com>
2019-01-06 02:20:11 +0700

    Deploy to Heroku ver1.0

--- server.py
+++ server.py
@@ -83,4 +83,7 @@ def show_subpath(subpath):
 
 
 if __name__ == '__main__':
-   app.run(debug = True)
+  app.run(debug = True)
+  # Bind to PORT if defined, otherwise default to 5000.
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
